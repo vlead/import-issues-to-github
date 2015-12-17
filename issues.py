ac82c1d7408975e1f0e0eb4810a8f7659d70d605
Yogesh Agrawal <yogeshiiith@gmail.com>
2015-12-17 06:58:10 -0500

    move variables to config files

--- issues.py
+++ issues.py
@@ -2,17 +2,17 @@ import os
 import sys
 import csv
 import json
+import urllib
+from config import *
 
 column = {}
-token = "a2bf7f64bd62187791b092989823f4dc16310cc8"
-githubUrl = "https://api.github.com/repos/ayogi/import-github-issues/issues"
 
 def defineTitle(header):
     index = 0
     for title in header:
         column[title] = index
         index+=1
-   return
+    return
 
 def printIssue(reader):
     issueNumber = 1
@@ -23,15 +23,30 @@ def printIssue(reader):
         issueNumber+=1
     return
 
+def postRequest(curl):
+    os.system(curl)
+    return
+
 def createIssue(row):
     dictionary = {}
-    dictionary["title"] = row[column["Subject"]]
-    dictionary["body"] = row[column["Description"]]
+    project = row[column["Project"]]
+    experimentName = row[column["Experiment name"]]
+    feature = row[column["Feature"]]
+    testStepNum = row[column["Test Step No"]]
+    testStepNum = testStepNum.zfill(2)
+    subject = "QA_%s_%s" %(experimentName, feature)
+    explink = urllib.quote(experimentName)
+    link =  "https://github.com/%s/%s/blob/master/test-cases/integration_test-cases/%s/%s_%s_%s.org" %(organization, project, explink, explink, testStepNum, feature)
+    
+    description = row[column["Description"]] + "\n Test Step Link:\n%s" %(link)
+    dictionary["title"] = subject
+    dictionary["body"] = description
     dictionary["labels"] = [row[column["Status"]], row[column["Severity"]], row[column["Category"]], row[column["Assigned by"]], row[column["Release Number"]], row[column["Start date"]], row[column["Project"]]]
     jsonString = json.dumps(dictionary)
+
     curl = """curl -i -H 'Authorization: token %s' -d '%s' %s""" %(token, jsonString, githubUrl)
-    print curl
-    os.system(curl)
+    postRequest(curl)
+    return
 
 def main(args):
     filePointer = open(args[1], 'r')
@@ -43,13 +58,6 @@ def main(args):
     createIssue(row)
     filePointer.close()
     return
-
-"""
-['#', 'Project', 'Tracker', 'Parent task', 'Status', 'Priority', 'Subject', 'Author', 'Assignee', 'Updated', 'Category', 'Target version', 'Start date', 'Due date', 'Estimated time', '% Done', 'Created', 'Closed', 'Related issues', 'Assigned by', 'Severity', 'Category', 'Release Number', 'Developer Comments', 'Test Step No', 'Description']
-
-curl = curl -i -H 'Authorization: token ba89586c2af27b18c180f4fb346bbba838532395' -d '{ "title": "QA_Defect_Structural Dynamics_100", "body": "%s", "labels": ["S2"]}' https://api.github.com/repos/ayogi/import-github-issues/issues
-%(body2)
-"""
 
 if __name__ == "__main__":
     main(sys.argv)
